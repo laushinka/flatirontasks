b837cfa46b678a817409caed86205c8a3a624500
Jonathan Alexander <jonathandalexander@gmail.com>
2016-07-12 19:26:56 -0400

    updated StudentsController.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,16 +1,37 @@
 class StudentsController < ApplicationController
-    def index
+  
+  def index
+   @students = Student.all
   end
 
   def new
+    @student = Student.new
+    # creates new student object to use in form_for
   end
 
   def create
+    @student = Student.create(student_params)
+  end
+
+  def show
+    @student = Student.find(params[:id])
   end
 
   def edit
+    @student = Student.find(params[:id])
   end
 
-  def create
+  def update
+    @student = Student.find(params[:id])
+  end
+
+  def destroy
+    @student.destroy
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name, :email, :password_digest)
   end
 end

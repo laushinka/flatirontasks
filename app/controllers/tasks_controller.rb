d4a331dfb68b9b863464a6185845aae554fa0a55
laushinka <laurie.malau@gmail.com>
2016-07-13 09:34:16 -0400

    Add controller methods

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,22 @@
 class TasksController < ApplicationController
+  def new
+    @task = Task.new
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def create
+    @task = Task.create(task_params)
+    redirect_to task_path
+  end
+
+  def index
+    @tasks = Task.all
+  end
+
+  def task_params
+    params.require(:task).permit(:name, :description, :time)
+  end
 end

66d5276456b96b9a564f3277a14ab6ce865a8564
Jonathan Alexander <jonathandalexander@gmail.com>
2016-07-13 09:34:44 -0400

    updates to student model, controller and views. added better_errors & binding_of_caller gems.

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,7 @@ class Student < ApplicationRecord
   has_many :student_assignments
   has_many :assignments, through: :student_assignments
   has_many :tasks
+
+  #validates_presence_of :name, :email, :password_disgest
+  #validates_uniqueness_of :email
 end
